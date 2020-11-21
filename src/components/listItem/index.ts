8231a09b47fe9064faa41552bd2facffb10c7ff2
Matthew Finucane <matfin@gmail.com>
2020-11-21 08:29:40 +0000

    feat: set style for invalid list items

--- src/components/listItem/index.ts
+++ src/components/listItem/index.ts
@@ -2,17 +2,31 @@ import template from './template';
 
 export class ListItemComponent extends HTMLElement {
   private deleteButton: HTMLButtonElement | null | undefined;
+  private listElement: HTMLLIElement | null | undefined;
   private clickDeleteCallback: ((id: string) => void) | null;
 
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
+
+    this.deleteButton = null;
+    this.listElement = null;
+
     this.onDeleteButtonClick = this.onDeleteButtonClick.bind(this);
     this.clickDeleteCallback = null;
   }
 
   static get observedAttributes(): string[] {
-    return ['id', 'title', 'onClickDelete'];
+    return ['id', 'invalid', 'title', 'onClickDelete'];
+  }
+
+  private render(): void {
+    const { shadowRoot } = this;
+    const titleNode = shadowRoot?.querySelector('[title]');
+
+    if (titleNode) {
+      titleNode.innerHTML = this.title;
+    }
   }
 
   setAttribute(name: string, value: string): void {
@@ -26,6 +40,8 @@ export class ListItemComponent extends HTMLElement {
         this.title = newVal;
       } else if (name === 'id') {
         this.id = newVal;
+      } else if (name === 'invalid') {
+        this.isInvalid = newVal !== null;
       }
     }
   }
@@ -36,18 +52,10 @@ export class ListItemComponent extends HTMLElement {
 
     shadowRoot?.appendChild(templateInstance);
     this.deleteButton = shadowRoot?.querySelector('[delete]');
+    this.listElement = shadowRoot?.querySelector('li');
     this.deleteButton?.addEventListener('click', this.onDeleteButtonClick);
 
     this.render();
-  }
-
-  render(): void {
-    const { shadowRoot } = this;
-    const titleNode = shadowRoot?.querySelector('[title]');
-
-    if (titleNode) {
-      titleNode.innerHTML = this.title;
-    }
   }
 
   disconnectedCallback(): void {
@@ -74,6 +82,18 @@ export class ListItemComponent extends HTMLElement {
 
   set title(title: string) {
     this.setAttribute('title', title);
+  }
+
+  get isInvalid(): boolean {
+    return this.hasAttribute('invalid');
+  }
+
+  set isInvalid(isInvalid: boolean) {
+    if (isInvalid) {
+      this.setAttribute('invalid', 'true');
+    } else {
+      this.removeAttribute('invalid');
+    }
   }
 
   set onClickDelete(onClickDelete: (id: string) => void) {
